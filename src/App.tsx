4c36e11f641d0c01b5826abe5c85978bb20fb137
Abdullah Azhar Khan <abdullahazharkhan1@gmail.com>
2025-04-23 22:09:16 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-yellow-300 text-black p-2 text-center'>CFG Derivation Game</h1>
+    </>
+  )
+}
+
+export default App
